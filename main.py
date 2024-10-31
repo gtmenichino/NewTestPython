f4a2474518665546992f525282f6510cf7a8be79
gtmenichinoUSACE <131197751+gtmenichinoUSACE@users.noreply.github.com>
2024-10-31 11:00:17 -0400

    Added requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+#pip freeze > requirements.txt
+#this creates a static copy of the current environment
+
+#pip install -r requirements.txt
+#this installs all the packages in the requirements.txt file, make sure you've created and activated the virtual environment first
+
 print("hello world")
